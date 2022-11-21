a5f22eac05395c63b798da19ed327ad9a555c4a6
Soaky01 <ovidiu@ciucu.ro>
2022-11-22 01:22:33 +0200

    hw1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -2,24 +2,25 @@ list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 print(list)
 
 # crescator
-list_ord = sorted(list)
-print(list_ord)
+list_cres = sorted(list)
+print(list_cres)
 
 # descrescator
 list_des = sorted(list, reverse=True)
 print(list_des)
 
 # elemente pare
-# list_par = list_ord[1::2]
+# list_par = list_cres[1::2]
 # print(list_par)
 print(sorted(list)[1::2])
 
 # elemente impare
-# list_impar = list_ord[::2]
+# list_impar = list_cres[::2]
 # print(list_impar)
 print(sorted(list)[::2])
 
 # multiplii de 3
 for i in range(len(list)):
     if (list[i] % 3 == 0):
-        print(list[i])
+        print(list[i], end=' ')
+
