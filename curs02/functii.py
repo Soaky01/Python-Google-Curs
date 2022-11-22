f18acc2e71d5db4d2d51c3679a179be5e5c2dbcc
Soaky01 <ovidiu@ciucu.ro>
2022-11-22 20:49:35 +0200

    curs2

---
+++ curs02/functii.py
@@ -0,0 +1,12 @@
+# print("")
+# input("")
+def functie_1(param_1, param_2, *args, **kwargs):
+    print(type(kwargs))
+    suma = param_1 + param_2
+    for i in args:
+        suma += i
+    for i in kwargs.values():
+        suma += i
+    return suma
+
+print(functie_1(1, 5, 4, 5, v=3, c=6))
