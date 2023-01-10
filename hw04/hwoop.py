9b97c4700bcb49db47623742bb2285854f9eddd2
Soaky01 <ovidiu@ciucu.ro>
2023-01-10 21:26:49 +0200

    '2023'

---
+++ hw04/hwoop.py
@@ -0,0 +1,20 @@
+     class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+         self.denominator = denominator                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
